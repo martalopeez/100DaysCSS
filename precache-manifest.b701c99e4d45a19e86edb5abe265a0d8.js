301d01127d01ffedd4a9c5f015fe4bf51d9b1e51
martalopeez <martalopeez88@gmail.com>
2023-02-01 17:36:05 +0100

    Deploy React app to GitHub Pages

---
+++ precache-manifest.b701c99e4d45a19e86edb5abe265a0d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdbcbbdb3bfcf92b506e",
+    "url": "/100DaysCSS/static/css/main.35ebeffc.chunk.css"
+  },
+  {
+    "revision": "bdbcbbdb3bfcf92b506e",
+    "url": "/100DaysCSS/static/js/main.bdbcbbdb.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/100DaysCSS/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/100DaysCSS/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "5e4b83ff29ebacc9cfee",
+    "url": "/100DaysCSS/static/js/runtime~main.5e4b83ff.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/100DaysCSS/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "29fdf39fe63f20b3908b4a917c57f101",
+    "url": "/100DaysCSS/index.html"
+  }
+];
