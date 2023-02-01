55c9346b53fb181d71b97bb448dedc52372a143d
martalopeez <martalopeez88@gmail.com>
2023-02-01 18:30:55 +0100

    adding fonts

--- 100-days-css/src/App.tsx
+++ 100-days-css/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"> 
+      <body className="App-header"> 
       <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" className='App-logo' alt='css'/>
-        <p className="font-bold text-xl">
+        <p>
           100 Days CSS Challenge
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='buttons'>
+          <button className='btn'>Day 1</button>
+          <button className='btn'>Day 2</button>
+        </div>
+      </body>
     </div>
   );
 }
