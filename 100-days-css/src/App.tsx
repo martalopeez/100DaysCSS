9178381a8650a0dc99e794c15f66f8961ab356e8
martalopeez <martalopeez88@gmail.com>
2023-02-01 19:33:38 +0100

    Adding Day 1

--- 100-days-css/src/App.tsx
+++ 100-days-css/src/App.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Day1 from './components/day1/Day1';
 
 function App() {
+  const [day, setDay] = useState(0);
   return (
     <div className="App">
       <body className="App-header"> 
       <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" className='App-logo' alt='css'/>
-        <p>
-          100 Days CSS Challenge
-        </p>
-        <div className='buttons'>
-          <button className='btn'>Day 1</button>
-          <button className='btn'>Day 2</button>
-        </div>
+        <p>100 Days CSS Challenge</p>
       </body>
+      <div>
+        <div className='buttons'>
+          <button onClick={() => setDay(1)} className='btn'>Day 1</button>
+          <button onClick={() => setDay(2)} className='btn'>Day 2</button>
+        </div>
+        {day == 1 && (
+          <Day1 />
+        )}
+      </div>
     </div>
   );
 }
---
+++ 100-days-css/src/components/day1/Day1.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Day1.css';
+
+function Day1() {
+    return (
+    <div className="frame">
+        <div className="center">
+            <div className="number">
+                <div className="one-one"></div>
+                <div className="one-two"></div>
+                <div className="zero-one"></div>
+                <div className="zero-two"></div>
+            </div>
+            <div className="divtext">
+              <span className="big">Days</span><br></br>
+              <span className="small">CSS CHALLENGE</span>
+            </div>
+        </div>
+      </div>
+    );
+  }
+  
+export default Day1;
