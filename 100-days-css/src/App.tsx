3ebd623cc3ed0aa0547ae3cdb677a94ac9a8cf75
martalopeez <martalopeez88@gmail.com>
2023-02-01 17:51:32 +0100

    Creating react app

---
+++ 100-days-css/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> 
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" className='App-logo' alt='css'/>
+        <p className="font-bold text-xl">
+          100 Days CSS Challenge
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
