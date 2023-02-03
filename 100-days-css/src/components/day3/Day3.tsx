05328f9801fb0afce787f2317166f2025ff5d07c
martalopeez <martalopeez88@gmail.com>
2023-02-03 21:01:46 +0100

    Adding day 3 (unfinished) dynamically

---
+++ 100-days-css/src/components/day3/Day3.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Day3.css';
+
+function Day3() {    
+    return (
+      <div className="frame">
+        <div className="center">
+          <div className="circle">
+            <div className="sky"></div>
+            <div className="sand"></div>
+            <div className="pyramid-left"></div>
+            <div className="pyramid-right"></div>
+            <div className="sun"></div>
+            <div className="shadow"></div>
+          </div>
+        </div>
+</div>
+    );
+}
+
+export default Day3;
